53b726ddad4fe590360038dd3791c8a5a4a3893d
Sinchana M P <sinchanamp1808@gmail.com>
2026-09-11 14:39:39 +0530

    Setup initial React client structure for customer portal

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style = {{ padding: "20px", fontFamily: "Arial"}}>
+      <h1>Vehicle Rental & Fleet Managemnet</h1>
+      <p>Customer Portal & Booking System</p>
+    </div>
+  );
+}
+
+export default App;
