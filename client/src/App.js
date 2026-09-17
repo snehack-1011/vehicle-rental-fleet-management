214c606c25b7f940a74e3a862116a609562aa94e
Divya m <divyamahesh576@gmail.com>
2026-09-17 11:00:23 +0530

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-
 import React from 'react';
 import Login from './components/Authentication/Login';
 import Register from './components/Authentication/Register';
+import Dashboard from './Frontend2/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign: 'center', marginTop: '20px' }}>Customer Portal</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+        Customer Portal
+      </h1>
+
       <Login />
-      <hr style={{ margin: '40px 0' }}/>
+
+      <hr style={{ margin: '40px 0' }} />
+
       <Register />
 
-      
+      <hr style={{ margin: '40px 0' }} />
+
+      <Dashboard />
     </div>
   );
 }
